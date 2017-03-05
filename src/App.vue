0139966f022a5643bb1f1cc764a313e92090a951
Stepan Postranecky <stepan.postranecky@gmail.com>
2017-03-05 12:29:06 +0100

    Added mainpage content

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-inverse navbar-fixed-top navbar-custom">
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle" data-toggle="collapse"
+                  data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
           </button>
-          <a class="navbar-brand" href="#">VIPO</a>
+          <a class="navbar-brand" href="#page-top">VIPO</a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="hidden">
+              <a href="#page-top"></a>
+            </li>
+            <li class="page-scroll">
+              <a href="#portfolio">Ceník</a>
+            </li>
+            <li class="page-scroll">
+              <a href="#about">Kontakt</a>
+            </li>
           </ul>
-        </div><!--/.nav-collapse -->
+        </div>
+        <!-- /.navbar-collapse -->
       </div>
+      <!-- /.container-fluid -->
     </nav>
     <header>
       <div class="container">
@@ -28,28 +37,85 @@
             <div class="intro-text">
               <span class="name">VIPO - Autodoprava</span>
               <hr class="star-light">
-              <span class="skills">Web Developer - Graphic Artist - User Experience Designer</span>
+              <span class="skills">Rychle - Bezpečně - Spolehlivě</span>
             </div>
           </div>
         </div>
       </div>
     </header>
     <div class="container">
-
-
-
+      <router-view></router-view>
     </div>
-    <!--<router-view></router-view>-->
+    <!-- Footer -->
+    <footer class="text-center">
+      <div class="footer-above">
+        <div class="container">
+          <div class="row">
+            <div class="footer-col col-md-4">
+              <h3>Adresa</h3>
+              <p>Autodoprava Postránecký<br>
+                Vinohrady 227<br>
+                68401 Kobeřice u Brna</p>
+            </div>
+            <div class="footer-col col-md-4">
+              <h3>Around the Web</h3>
+              <ul class="list-inline">
+                <li>
+                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-col col-md-4">
+              <h3>O nás</h3>
+              <p>Freelance is a free to use, open source Bootstrap theme created by <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-below">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              Copyright &copy; Your Website 2016
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+      <a class="btn btn-primary" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+      </a>
+    </div>
+
   </div>
 </template>
 
 <script>
-
+  require('bootstrap');
+  require('./assets/startbootstrap-freelancer-gh-pages/js/freelancer')
 </script>
 
 <style>
   @import url('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
-  @import url('./assets/startbootstrap-freelancer-gh-pages/css/freelancer.min.css');
+  @import url('./assets/startbootstrap-freelancer-gh-pages/css/freelancer.css');
+  @import url('./assets/startbootstrap-freelancer-gh-pages/vendor/font-awesome/css/font-awesome.css');
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+  @import url('https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
 
   body {
     padding-top: 50px;
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,3 +1,73 @@
 <template>
-  <div>Hello</div>
+  <div>
+    <section id="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2>Co odvezeme</h2>
+            <hr class="star-primary">
+          </div>
+        </div>
+        <div class="row row-graphics">
+          <div class="col-sm-4 portfolio-item text-center">
+            <img src="../assets/img/euro-pallet-161949_960_720.png" class="img-responsive" alt="">
+            <h3>7 Europalet</h3>
+          </div>
+          <div class="col-sm-4 portfolio-item text-center">
+            <img src="../assets/img/weight-tool-pink.png" class="img-responsive" alt="">
+            <h3>1,2 tuny</h3>
+          </div>
+          <div class="col-sm-4 portfolio-item text-center">
+            <img src="../assets/img/ruler.png" class="img-responsive" alt="">
+            <h3>4,5m na délku</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- About Section -->
+    <section class="success" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2>O nás</h2>
+            <hr class="star-light">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-lg-offset-2">
+            <p>Nabízíme Vám vnitrostátní kvalitní, rychlou a spolehlivou dopravu čehokoliv a kamkoliv.<br>
+              Klademe velký důraz na spolehlivost naší firmy, takže se nemusíte obávat případných selhání nebo zanedbání našich závazků. Vážíme si každého klienta.</p>
+          </div>
+          <div class="col-lg-4">
+            <p>Pokud Vám není něco jasné a potřebujete ohedně dopravy poradit. Rádi Vám na Vaše otázky odpovíme.
+              Kontaktovat nás můžete e-mailem nebo telefonicky.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+
+<style>
+  .portfolio-item img {
+    max-height: 239px;
+    margin: 0 auto;
+  }
+
+  .row-graphics {
+    display: table-row;
+  }
+
+  .portfolio-item {
+    height: 290px;
+    display: table-cell;
+  }
+
+  .portfolio-item h3 {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    text-align: center;
+  }
+</style>
