30c0e970b5e512b1c591cdddc4fbc7eca2f06cad
Stepan Postranecky <stepan.postranecky@gmail.com>
2017-03-05 19:38:25 +0100

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,20 @@
 <template>
   <div id="app">
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header page-scroll">
-          <button type="button" class="navbar-toggle" data-toggle="collapse"
-                  data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-          </button>
-          <a class="navbar-brand" href="#page-top">VIPO</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="hidden">
-              <a href="#page-top"></a>
-            </li>
-            <li class="page-scroll">
-              <a href="#portfolio">Ceník</a>
-            </li>
-            <li class="page-scroll">
-              <a href="#about">Kontakt</a>
-            </li>
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-      </div>
-      <!-- /.container-fluid -->
-    </nav>
-    <header>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <img class="img-responsive" src="./assets/img/IMG_1939.png" alt="">
-            <div class="intro-text">
-              <span class="name">VIPO - Autodoprava</span>
-              <hr class="star-light">
-              <span class="skills">Rychle - Bezpečně - Spolehlivě</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-    <div class="container">
-      <router-view></router-view>
-    </div>
-    <!-- Footer -->
-    <footer class="text-center">
-      <div class="footer-above">
-        <div class="container">
-          <div class="row">
-            <div class="footer-col col-md-4">
-              <h3>Adresa</h3>
-              <p>Autodoprava Postránecký<br>
-                Vinohrady 227<br>
-                68401 Kobeřice u Brna</p>
-            </div>
-            <div class="footer-col col-md-4">
-              <h3>Around the Web</h3>
-              <ul class="list-inline">
-                <li>
-                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
-                </li>
-                <li>
-                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
-                </li>
-                <li>
-                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
-                </li>
-                <li>
-                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
-                </li>
-                <li>
-                  <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
-                </li>
-              </ul>
-            </div>
-            <div class="footer-col col-md-4">
-              <h3>O nás</h3>
-              <p>Freelance is a free to use, open source Bootstrap theme created by <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="footer-below">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              Copyright &copy; Your Website 2016
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
-      <a class="btn btn-primary" href="#page-top">
-        <i class="fa fa-chevron-up"></i>
-      </a>
-    </div>
-
+    <vipoMenu></vipoMenu>
+    <router-view></router-view>
+    <vipoFooter></vipoFooter>
   </div>
 </template>
 
 <script>
-  require('bootstrap');
-  require('./assets/startbootstrap-freelancer-gh-pages/js/freelancer')
+  import menu from './components/menu.vue'
+  import footer from './components/footer.vue'
+  export default{
+    components: {
+      vipoMenu: menu,
+      vipoFooter: footer
+    }
+  }
 </script>
 
 <style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,73 +1,121 @@
 <template>
   <div>
-    <section id="portfolio">
+    <header>
       <div class="container">
         <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2>Co odvezeme</h2>
-            <hr class="star-primary">
-          </div>
-        </div>
-        <div class="row row-graphics">
-          <div class="col-sm-4 portfolio-item text-center">
-            <img src="../assets/img/euro-pallet-161949_960_720.png" class="img-responsive" alt="">
-            <h3>7 Europalet</h3>
-          </div>
-          <div class="col-sm-4 portfolio-item text-center">
-            <img src="../assets/img/weight-tool-pink.png" class="img-responsive" alt="">
-            <h3>1,2 tuny</h3>
-          </div>
-          <div class="col-sm-4 portfolio-item text-center">
-            <img src="../assets/img/ruler.png" class="img-responsive" alt="">
-            <h3>4,5m na délku</h3>
+          <div class="col-lg-12">
+            <img class="img-responsive" src="../assets/img/IMG_1939.png" alt="">
+            <div class="intro-text">
+              <span class="name">VIPO - Autodoprava</span>
+              <hr class="star-light">
+              <span class="skills">Rychle - Bezpečně - Spolehlivě</span>
+            </div>
           </div>
         </div>
       </div>
-    </section>
-    <!-- About Section -->
-    <section class="success" id="about">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2>O nás</h2>
-            <hr class="star-light">
+    </header>
+    <div class="container">
+      <section id="portfolio">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <h2>Co odvezeme</h2>
+              <hr class="star-primary">
+            </div>
+          </div>
+          <div class="row row-graphics row-eq-height">
+            <div class="col-sm-4 portfolio-item text-center">
+              <img src="../assets/img/euro-pallet-161949_960_720.png" class="img-responsive  center-block" alt="">
+              <div class="portfolio-text"><h3>7 Europalet</h3></div>
+            </div>
+            <div class="col-sm-4 portfolio-item text-center">
+              <img src="../assets/img/weight-tool-pink.png" class="img-responsive center-block" alt="">
+              <div class="portfolio-text"><h3>1,2 tuny</h3></div>
+            </div>
+            <div class="col-sm-4 portfolio-item text-center">
+              <img src="../assets/img/ruler.png" class="img-responsive  center-block" alt="">
+              <div class="portfolio-text"><h3>4,5m na délku</h3></div>
+            </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-4 col-lg-offset-2">
-            <p>Nabízíme Vám vnitrostátní kvalitní, rychlou a spolehlivou dopravu čehokoliv a kamkoliv.<br>
-              Klademe velký důraz na spolehlivost naší firmy, takže se nemusíte obávat případných selhání nebo zanedbání našich závazků. Vážíme si každého klienta.</p>
+      </section>
+      <!-- About Section -->
+      <section class="success" id="about">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <h2>O nás</h2>
+              <hr class="star-light">
+            </div>
           </div>
-          <div class="col-lg-4">
-            <p>Pokud Vám není něco jasné a potřebujete ohedně dopravy poradit. Rádi Vám na Vaše otázky odpovíme.
-              Kontaktovat nás můžete e-mailem nebo telefonicky.</p>
+          <div class="row">
+            <div class="col-lg-4 col-lg-offset-2">
+              <p>Nabízíme Vám vnitrostátní kvalitní, rychlou a spolehlivou dopravu čehokoliv a kamkoliv.<br>
+                Klademe velký důraz na spolehlivost naší firmy, takže se nemusíte obávat případných selhání nebo
+                zanedbání našich závazků. Vážíme si každého klienta.</p>
+            </div>
+            <div class="col-lg-4">
+              <p>Pokud Vám není něco jasné a potřebujete ohedně dopravy poradit. Rádi Vám na Vaše otázky odpovíme.
+                Kontaktovat nás můžete e-mailem nebo telefonicky.</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
+<script>
 
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+  ga('create', 'UA-92999099-1', 'auto');
+  ga('send', 'pageview');
+
+
+</script>
 
 <style>
-  .portfolio-item img {
-    max-height: 239px;
-    margin: 0 auto;
+  @media only screen and (min-width: 1024px) {
+    .portfolio-item img {
+      max-height: 80%;
+      max-width: 80%;
+      padding-bottom: 50px;
+    }
+
+    .row-eq-height {
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+    }
+
+    .portfolio-item {
+      position: relative;
+    }
+
+    .portfolio-text {
+      position: absolute;
+      margin-left: auto;
+      margin-right: auto;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+
+    .portfolio-text h3 {
+      margin-top: 30px;
+    }
   }
 
-  .row-graphics {
-    display: table-row;
-  }
 
-  .portfolio-item {
-    height: 290px;
-    display: table-cell;
-  }
-
-  .portfolio-item h3 {
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-    text-align: center;
-  }
 </style>
